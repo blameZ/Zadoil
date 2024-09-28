83ff849c84e7bd3e504f563ba302899fcee7e3e4
Bartosz Sermak <bartoszsermak@MacBook-Air-Bartosz.local>
2024-09-28 23:22:15 +0200

    JS changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+	const currentYearElement = document.getElementById('current-year')
+	const currentYear = new Date().getFullYear()
+	currentYearElement.textContent = currentYear
+})
+
+document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
 	const allNavItems = document.querySelectorAll('.nav-link')
 	const navList = document.querySelector('.navbar-collapse')
@@ -28,6 +34,31 @@ document.addEventListener('DOMContentLoaded', function () {
 			navLinks.forEach(nav => nav.classList.remove('active'))
 
 			this.classList.add('active')
+		})
+	})
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	const navLinks = document.querySelectorAll('.nav-link')
+	const sections = document.querySelectorAll('section')
+
+	window.addEventListener('scroll', function () {
+		let currentSection = ''
+
+		sections.forEach(section => {
+			const sectionTop = section.offsetTop
+			const sectionHeight = section.clientHeight
+
+			if (pageYOffset >= sectionTop - sectionHeight / 3) {
+				currentSection = section.getAttribute('id')
+			}
+		})
+
+		navLinks.forEach(link => {
+			link.classList.remove('active')
+			if (link.getAttribute('href').includes(currentSection)) {
+				link.classList.add('active')
+			}
 		})
 	})
 })
