a2b875e1562ccd1d3cd7b8f841a240b61fd0baec
Bartosz Sermak <bartoszsermak@MacBook-Air-Bartosz.local>
2025-08-02 23:36:54 +0200

    Added logic for nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,16 +2,31 @@ document.querySelectorAll('.nav-item.dropdown').forEach(dropdown => {
 	const link = dropdown.querySelector('a')
 	const menu = dropdown.querySelector('.dropdown-menu')
 
-	dropdown.addEventListener('mouseenter', () => {
-		menu.classList.add('show')
-	})
-	dropdown.addEventListener('mouseleave', () => {
-		menu.classList.remove('show')
-	})
+	if (window.innerWidth > 768) {
+		const dropdowns = document.querySelectorAll('.nav-item.dropdown')
 
-	link.addEventListener('click', e => {
-		menu.classList.toggle('show')
-	})
+		dropdowns.forEach(dropdown => {
+			const menu = dropdown.querySelector('.dropdown-menu')
+			let timeout
+
+			dropdown.addEventListener('mouseenter', () => {
+				clearTimeout(timeout)
+				menu.style.display = 'block'
+			})
+
+			dropdown.addEventListener('mouseleave', () => {
+				timeout = setTimeout(() => {
+					menu.style.display = 'none'
+				}, 100)
+			})
+		})
+
+		dropdowns.forEach(dropdown => {
+			dropdown.addEventListener('click', () => {
+				menu.style.display = 'none'
+			})
+		})
+	}
 
 	menu.querySelectorAll('a').forEach(itemLink => {
 		itemLink.addEventListener('click', () => {
